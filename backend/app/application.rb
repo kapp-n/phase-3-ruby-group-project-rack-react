fa42d32ac229c467f59020a955089819db60aaa7
Mollie Dickenson <molliedickenson@gmail.com>
2021-06-12 22:29:51 -0400

    total route functionality, css added

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -1,4 +1,5 @@
 class Application
+require 'pry'
 
   def call(env)
     resp = Rack::Response.new
@@ -8,12 +9,15 @@ class Application
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
 
     elsif req.path.match(/shelves/)
-      shelves = Shelf.all
-      return [200, {'Content-Type' => 'application/json'}, [ shelves.to_json({:include => :books}) ] ]
-  
-
+        shelves = Shelf.all
+        return [200, {'Content-Type' => 'application/json'}, [ shelves.to_json({:include => :books}) ] ]
+    elsif req.path.match(/book/)
+        books = Book.all
+        book_id = req.path.split("/book/").last
+        return [200, { 'Content-Type' => 'application/json' }, [ books.find_by(id: book_id).to_json ]]
     else
       resp.write "Path Not Found"
+      binding.pry
 
     end
 
