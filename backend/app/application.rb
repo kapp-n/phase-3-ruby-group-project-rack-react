ea4ab8b9c9cf677f5840dd0b021667a648d8b51b
Mollie Dickenson <molliedickenson@gmail.com>
2021-06-13 15:16:08 -0400

    finished delete functionality for Shelf

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -9,8 +9,21 @@ require 'pry'
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
 
     elsif req.path.match(/shelves/)
-        shelves = Shelf.all
-        return [200, {'Content-Type' => 'application/json'}, [ shelves.to_json({:include => :books}) ] ]
+      if req.env['REQUEST_METHOD']=="POST"
+        input = JSON.parse(req.body.read)
+        shelf = Shelf.create(genre: input["genre"])
+        return [200, {'Content-Type' => 'application/json'}, [ shelf.to_json({:include => :books}) ]]
+      elsif req.env['REQUEST_METHOD']=="DELETE"
+        input = JSON.parse(req.body.read)
+        shelf = Shelf.find_by(id: input)
+        shelf.destroy
+        return [200, {'Content-Type' => 'application/json'}, [ {:message => "deleted"}.to_json ] ]
+      end
+    shelves = Shelf.all
+    return [200, {'Content-Type' => 'application/json'}, [ shelves.to_json({:include => :books}) ] ]
+  
+
+
     elsif req.path.match(/book/)
         books = Book.all
         book_id = req.path.split("/book/").last
