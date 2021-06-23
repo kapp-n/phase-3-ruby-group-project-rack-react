7536b6860e850651847670db2e38971b222002c6
Mollie Dickenson <molliedickenson@gmail.com>
2021-06-23 14:10:40 -0400

    fixed delete function

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -26,29 +26,36 @@ require 'pry'
         input = JSON.parse(req.body.read)
         shelf = Shelf.find_by(id: input)
         shelf.destroy
-        return [200, {'Content-Type' => 'application/json'}, [ {:message => "deleted"}.to_json ] ]
+        return [200, {'Content-Type' => 'application/json'}, [ {:message => "deleted"}.to_json ]]
       end
 
     shelves = Shelf.all
-    return [200, {'Content-Type' => 'application/json'}, [ shelves.to_json({:include => :books}) ] ]
+    return [200, {'Content-Type' => 'application/json'}, [ shelves.to_json({:include => :books}) ]]
   
 
     elsif req.path.match(/book/)
+
 
       if req.env['REQUEST_METHOD']=="DELETE"
       
         input = JSON.parse(req.body.read)
         book = Book.find_by(id: input)
         book.destroy
+        shelves = Shelf.all
+        return [200, {'Content-Type' => 'application/json'}, [shelves.to_json({:include => :books})]]
+
       else
         books = Book.all
         book_id = req.path.split("/book/").last
         return [200, { 'Content-Type' => 'application/json' }, [ books.find_by(id: book_id).to_json ]]
       end
 
+
+
+
     else
       resp.write "Path Not Found"
-      binding.pry
+      
 
     end
 
--- backend/app/book.rb
+++ backend/app/book.rb
@@ -1,3 +1,16 @@
+
 class Book < ActiveRecord::Base
     belongs_to :shelf
+
+    def self.long_books
+        self.all.select{ |b| b.pages > 400 }
+    end
+
+    def book_name
+        title = self.name
+        author = self.author
+        shelf = self.shelf.genre
+        "#{title} #{author} #{shelf}"
+    end
+
 end
